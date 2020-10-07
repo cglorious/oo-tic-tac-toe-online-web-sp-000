6bc8113c1842b0e1c3865ac7e1d328ee9eb6e5ef
Catherine Gloria <catherinegloria@gmail.com>
2020-10-07 18:21:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,15 @@ WIN_COMBINATIONS = [
 
   def initialize(board)
     board = Array.new(9, " ")
-    @board = board 
+    @board = board
+  end
+
+  def display_board
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 
 end
